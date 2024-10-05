078f309d96f1999d958c7643f2fb0698c12344c7
akmes <gabriel.silva.sergio1@gmail.com>
2024-10-05 17:08:53 -0300

    add login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,5 @@
 import { LoginForm } from "@/components/Login/login-form";
-
+//criando sistema de login
 const LoginPage = () => {
   return (
     <div className="min-h-screen flex items-center justify-center">
